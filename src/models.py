b4294906d13d3c26526bab68c16d002147608628
DCardigo <123099651+DCardigo@users.noreply.github.com>
2023-07-28 10:41:27 +0000

    initial commit

--- src/models.py
+++ src/models.py
@@ -2,6 +2,32 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Character(db.Model):
+    __tablename__ = 'characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=True)
+    birth_year = db.Column(db.Integer, nullable=True)
+    gender = db.Column(db.String(250), nullable=True)
+    height = db.Column(db.Integer, nullable=True)
+    skin_color = db.Column(db.String(250), nullable=True)
+    eye_color = db.Column(db.String(250), nullable=True)
+    # favoritos = db.relationship ('Favoritos', backref= 'characters', lazy=True)
+    def __repr__(self):
+        return '<Characters %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name" : self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender, 
+            "height": self.height,  
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color
+            # do not serialize the password, its a security breach
+        }
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -9,7 +35,7 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
@@ -17,3 +43,4 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
